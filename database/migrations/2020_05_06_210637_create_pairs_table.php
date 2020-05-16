42b6d0ec62feffdcb83a606f6890e8a6c74b2be7
habatynchik <habatynchik@gmail.com>
2020-05-16 20:04:22 +0300

    migration and DB change

--- database/migrations/2020_05_06_210637_create_pairs_table.php
+++ database/migrations/2020_05_06_210637_create_pairs_table.php
@@ -17,10 +17,10 @@ class CreatePairsTable extends Migration
             $table->id();
             $table->integer('id_user_student');
             $table->integer('id_schedule');
-            $table->integer('nubmer');
+            $table->integer('number');
             $table->date('date');
             $table->time('arrive_time');
-            $table->string('link');
+            $table->string('link')->nullable();;
         });
     }
 
