7b441d853ccc7a59701152dde1a3426ff376818e
Vladislav Redko <vladislav.redko@extreme-idea.com>
2020-05-16 21:09:18 +0300

    SHO KAVO

--- resources/js/components/Pairs/Main.js
+++ resources/js/components/Pairs/Main.js
@@ -1,60 +1,56 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, setState, useState } from 'react';
 import ReactDOM from 'react-dom';
- 
-/* Main Component */
-class Main extends Component {
- 
-  constructor() {
-   
-    super();
-    //Initialize the state in the constructor
-    this.state = {
-        pairs: [],
-    }
-  }
-  /*componentDidMount() is a lifecycle method
-   * that gets called after the component is rendered
-   */
-  componentDidMount() {
-    /* fetch API in action */
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+function Main(){
+  const [pairs, setPairs] = useState([]);
+
+  useEffect(() => {
     fetch('/api/pairs')
-        .then(response => {
-            return response.json();
-        })
-        .then(pairs => {
-            //Fetched pair is stored in the state
-            this.setState({ pairs });
-        });
-  }
- 
- renderpairs() {
-    return this.state.pairs.map(pair => {
-        return (
-            /* When using list you need to specify a key
-             * attribute that is unique for each list item
-            */
-            <li key={pair.id} >
-                { pair.number } 
-            </li>      
-        );
+      .then(response => {
+          return response.json();
+      })
+      .then(result => {
+          //Fetched pair is stored in the state
+          setPairs(result);
+      });
+  });
+
+  function renderPairs (){
+    return pairs.map(pair => {
+      return (
+        <ListItem button key={ pair.id }>
+          <ListItemText primary={ pair.number } />
+        </ListItem>
+      );
     })
   }
-   
-  render() {
-    return (
-        <div>
-              <ul>
-                { this.renderpairs() }
-              </ul> 
-            </div> 
-       
-    );
-  }
+
+  const classes = useStyles();
+  return(
+    <div className={classes.root}>
+        <List>
+           { renderPairs() }
+        </List>
+    </div>
+  );
 }
 
 export default Main;
 
- 
 if (document.getElementById('pairs')) {
     ReactDOM.render(<Main />, document.getElementById('pairs'));
 }
