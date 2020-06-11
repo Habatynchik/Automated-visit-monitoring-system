e0ffdfc1a560726be28d57feee2ba7baff829717
Vladislav Redko <vladislav.redko@extreme-idea.com>
2020-06-11 05:06:00 +0300

    fixed:

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -8,11 +8,6 @@ use UserController;
 
 class RouteController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     public function call($controller, $method){
         $className = "App\Http\Controllers\\" . ucfirst($controller) . "Controller";
         $controller = new $className;
